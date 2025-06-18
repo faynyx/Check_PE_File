ce332c9ebd0556ef0d261c34c8148dfc9f9daec8
faynyx <faynyx30@gmail.com>
2025-06-18 19:05:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,10 @@
 #include <tchar.h>
 #include <string.h>
 
+void Error_193(HANDLE filehandle) {
+
+}
+
 void TryCreateProcessWithDebug(LPCSTR filePath) {
     STARTUPINFOA si;
     PROCESS_INFORMATION pi;
@@ -22,29 +26,45 @@ void TryCreateProcessWithDebug(LPCSTR filePath) {
         TerminateProcess(pi.hProcess, 0);
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
-    } else {
-        DWORD err = GetLastError();
-        printf("[FAIL ] %s (Error: %lu)\n", filePath, err);
+    }
+    else {
+        DWORD err = GetLastError(); // Error Check
+        LPVOID lpMsgBuf;
+
+        FormatMessageA(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,
+            err,
+            MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+            (LPSTR)&lpMsgBuf,
+            0, NULL);
+
+        printf("[FAIL ] %s (Error: %lu : %s)\n", filePath, err, (char *)lpMsgBuf); // Error Code 
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char* argv[]) {
+    if (argc != 2) { // Directory Search
         printf("Usage: %s <directory_path>\n", argv[0]);
         return 1;
     }
 
     char searchPath[MAX_PATH];
-    snprintf(searchPath, MAX_PATH, "%s\\*.exe", argv[1]);
+    snprintf(searchPath, MAX_PATH, "%s\\*", argv[1]);
 
     WIN32_FIND_DATAA findFileData;
     HANDLE hFind = FindFirstFileA(searchPath, &findFileData);
-    if (hFind == INVALID_HANDLE_VALUE) {
+
+    if (hFind == INVALID_HANDLE_VALUE) { // File Search
         printf("No .exe files found or invalid directory: %s\n", argv[1]);
         return 1;
     }
 
-    do {
+    do { 
+        if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) // Directory Path Remove
+            continue;
+
+        printf("%s\n", searchPath);
         char fullPath[MAX_PATH];
         snprintf(fullPath, MAX_PATH, "%s\\%s", argv[1], findFileData.cFileName);
         TryCreateProcessWithDebug(fullPath);
